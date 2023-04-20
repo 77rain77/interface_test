fb9233f1cd375ff073602efa915da0b3d8a1ddf8
RAIN_xr <rain141716@163.com>
2023-04-20 11:55:47 +0800

    问题一

--- demo/question_1.py
+++ demo/question_1.py
@@ -1,5 +1,28 @@
-l1=[1,2,3]
-l2=[1,3,4]
-l3=l1+l2
-print(sorted(l3))
-#舰载机了空手道解放咖啡机
+import datetime
+from datetime import time
+from threading import Thread
+
+
+def test():
+    for i in range(20):
+        print(i)
+time1=datetime.datetime.now()
+for i in range(5):
+    thread01 = Thread(target=test)
+    thread01.start()
+    thread01.join()
+time2=datetime.datetime.now()
+time=time2-time1
+print("多线程使用时间",time)
+
+
+
+
+
+# time1=datetime.datetime.now()
+# for i in range(20):
+#     print(i)
+# time2=datetime.datetime.now()
+# time=time2-time1
+# print("单线程使用时间",time)
+
